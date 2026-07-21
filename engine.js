0d683cef59913a019a77cef6dbac6e77a4daa3ef
Harshill <Harshillarora376@gmail.com>
2026-07-21 23:25:31 +0530

    added button click functionality

--- engine.js
+++ engine.js
@@ -1,1 +1,6 @@
 let button = document.getElementById('btn')
+let messageBox = document.getElementById('msg-box')
+
+button.addEventListener('click', function(){
+    messageBox.innerText = "The button was clicked!!";
+})
