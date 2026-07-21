22b605197f3574c8375cda139bf844c31b0ad0a8
Harshill <Harshillarora376@gmail.com>
2026-07-21 23:28:02 +0530

    converted button element call from id to class

--- engine.js
+++ engine.js
@@ -1,5 +1,5 @@
-let button = document.getElementById('btn')
-let messageBox = document.getElementById('msg-box')
+let button = document.getElementsByClassName('btn')
+let messageBox = document.getElementsByClassName('msg-box')
 
 button.addEventListener('click', function(){
     messageBox.innerText = "The button was clicked!!";
